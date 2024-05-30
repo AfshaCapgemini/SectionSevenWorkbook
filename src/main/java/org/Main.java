1b9b6c96aa3184c67e977c6a4b1f311bc205f775
Afsha Hossain <afsha.hossain@capgemini.com>
2024-05-30 17:26:48 +0100

    Complete Workbook 7.4 - Create copy constructor

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -5,30 +5,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        For section 7.1:
-        Person person = new Person();
-        person.name = "Mother Teresa";
-        person.nationality = "Macedonia";
-        person.dateOfBirth = "24 Aug 1910";
-        person.passport = new String[] {person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 9;
-        */
+        Person person = new Person("Florence Nightingale", "English", "12 May 1820", 9);
 
-        Person person = new Person("Mother Teresa", "Macedonia", "24 Aug 1910", 9);
-        person.setSeatNumber(5);
+        Person twin = new Person(person); // creates a copy of the first object
+        twin.setName("Afsha Nightingale"); // updates name field of the second object
+        twin.setSeatNumber(10); // updates the seat number of the second object
 
-
-        /*
-        For section 7.1:
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
-         */
 
         System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: " + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
+        System.out.println("Name: " + twin.getName() + "\n" + "Nationality: " + twin.getNationality() + "\n" + "Date of Birth: " + twin.getDateOfBirth() + "\n" + "Seat Number: " + twin.getSeatNumber() + "\n");
 
     }
 }
